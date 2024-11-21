dfe17a473385fedee2ce7fa138e2fd26c5e729c8
demuelle <dmueller@trilogyed.com>
2024-11-21 16:02:25 -0600

    Adds gitignore

--- src/pages/FilmSearch.tsx
+++ src/pages/FilmSearch.tsx
@@ -63,7 +63,7 @@ const FilmSearch = () => {
             onChange={(e) => setSearchInput(e.target.value)}
           />
           <button type='submit' id='searchBtn'>
-            Search
+            Find That Film!
           </button>
         </form>
       </section>
